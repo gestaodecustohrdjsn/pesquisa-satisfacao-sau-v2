69f71ee30a63baf17424d918edf69d7f88b325a8
gestaodecustohrdjsn <gestaodecustohrdjsn@gmail.com>
2026-01-15 17:40:27 -0400

    Add 'comImagens' property to options object
    Added a new property 'comImagens' to the options object.

--- js/app.js
+++ js/app.js
@@ -61,8 +61,9 @@ const perguntas = [
     texto: "5 - Você teve algum problema ou dificuldade durante o atendimento?",
     comImagens: true,  // ← ADICIONE ESTA LINHA
     opcoes: [
-      { valor: "Sim", label: "Tive um problema ou dificuldade!", imagem: "images/dificuldade.png" },  // ← MUDE
-      { valor: "Não", label: "Nenhum problema, tudo certo!", imagem: "images/ok.png" }  // ← MUDE
+      { valor: "Sim", label: "Tive um problema ou dificuldade!", imagem: "images/dificuldade.png" },
+      { valor: "Não", label: "Nenhum problema, tudo certo!", imagem: "images/ok.png" }
+
     ],
     condicional: true
   }
